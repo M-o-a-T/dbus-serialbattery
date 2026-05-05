61f5db022572df6d03a3c17d589f968ae4ae4142
Matthias Urlichs <matthias@urlichs.de>
2026-05-05 07:12:15 +0200

    Don't log every second

--- dbus-serialbattery/bms/jkbms_pb.py
+++ dbus-serialbattery/bms/jkbms_pb.py
@@ -313,7 +313,7 @@ class Jkbms_pb(Battery):
         discharge = unpack_from("<B", status_data, 199)[0]
         heat = unpack_from("<B", status_data, 215)[0]
 
-        logger.info("bal: " + str(bal) + " charge: " + str(charge) + " discharge: " + str(discharge) + " heat: " + str(heat))
+        # logger.info("bal: " + str(bal) + " charge: " + str(charge) + " discharge: " + str(discharge) + " heat: " + str(heat))
 
         self.charge_fet = 1 if charge != 0 else 0
         self.discharge_fet = 1 if discharge != 0 else 0
